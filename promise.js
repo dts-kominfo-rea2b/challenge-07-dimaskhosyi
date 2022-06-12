9577cae0365c5071d0b5d3694f6da0fb92b22e05
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2022-06-12 14:34:53 +0700

    Done

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const dataBaru = []
+
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if(emosi === undefined){
+      console.log('Maaf Error!')
+    }else{
+      if(emosi === 'marah'){
+        promiseTheaterIXX()
+          .then(rawIIX => rawIIX.filter((i) => i.hasil === emosi))
+          .then(hasilIXX => {
+            promiseTheaterVGC()
+              .then(rawVGC => rawVGC.filter((i) => i.hasil === emosi))
+              .then(hasilAkhir => resolve(hasilAkhir.length + hasilIXX.length))
+              .catch(error => error)    
+          })
+          .catch(error => error)
+      }else if(emosi === 'tidak marah'){
+        promiseTheaterIXX()
+          .then(rawIIX => rawIIX.filter((i) => i.hasil === emosi))
+          .then(hasilIXX => {
+            promiseTheaterVGC()
+              .then(rawVGC => rawVGC.filter((i) => i.hasil === emosi))
+              .then(hasilAkhir => resolve(hasilAkhir.length + hasilIXX.length))
+              .catch(error => error)    
+          })
+          .catch(error => error)
+      }
+    }
+  })
+}
 
 module.exports = {
   promiseOutput,
